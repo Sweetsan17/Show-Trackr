f4fa45ec83f03698692cbd8bf586ad5e34048d03
Sweetsan <coderintheworldhack100@gmail.com>
2026-04-09 08:14:37 +0530

    Add the eventlistener

--- JS/login.js
+++ JS/login.js
@@ -2,3 +2,13 @@ const form = document.querySelector(`#form`);
 const email = document.querySelector(`#email-address`);
 const password = document.querySelector(`#password`);
 
+form.removeEventListener(`submit`,(e)=>{
+    e.preventDefault();
+    validateInputs();
+})
+
+function validateInputs(){
+    const emailVal = email.value.trim();
+    const passwordVal = password.value.trim();
+    
+}
